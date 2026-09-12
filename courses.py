e3d768d750145bde36a4f3291ecea5c4a47cccc1
mismilum2008-ux <mis.milum2008@gmail.com>
2026-09-12 22:08:18 +0700

    Create courses.py

---
+++ courses.py
@@ -0,0 +1,243 @@
+# Database Mata Kuliah S1 Sistem Informasi
+# Universitas Terbuka
+
+COURSES = {
+    "MKWN4101": {
+        "nama": "Pendidikan Agama Islam",
+        "sks": 3,
+    },
+    "MKWN4109": {
+        "nama": "Pendidikan Kewarganegaraan",
+        "sks": 2,
+    },
+    "STMA4112": {
+        "nama": "Matematika Dasar",
+        "sks": 3,
+    },
+    "STSI4101": {
+        "nama": "Pengantar Sistem Informasi",
+        "sks": 3,
+    },
+    "STSI4102": {
+        "nama": "Algoritma dan Pemrograman",
+        "sks": 3,
+    },
+    "STSI4103": {
+        "nama": "Sistem Operasi",
+        "sks": 3,
+    },
+
+    "MKDI4201": {
+        "nama": "Bahasa Inggris",
+        "sks": 3,
+    },
+    "MKKI4201": {
+        "nama": "Pengantar Statistika",
+        "sks": 3,
+    },
+    "MKWN4108": {
+        "nama": "Bahasa Indonesia",
+        "sks": 2,
+    },
+    "MKWN4110": {
+        "nama": "Pancasila",
+        "sks": 2,
+    },
+    "STMA4113": {
+        "nama": "Aljabar Linear Elementer",
+        "sks": 2,
+    },
+    "STSI4104": {
+        "nama": "Struktur Data",
+        "sks": 3,
+    },
+    "STSI4105": {
+        "nama": "Basis Data",
+        "sks": 3,
+    },
+
+    "MKDI4202": {
+        "nama": "Belajar di Era Digital",
+        "sks": 3,
+    },
+    "STIK4111": {
+        "nama": "Dasar Pemrograman R",
+        "sks": 3,
+    },
+    "STSI4106": {
+        "nama": "Logika Informatika",
+        "sks": 3,
+    },
+    "STSI4201": {
+        "nama": "Pemrograman Berbasis Desktop",
+        "sks": 3,
+    },
+    "STSI4202": {
+        "nama": "Rekayasa Perangkat Lunak",
+        "sks": 3,
+    },
+    "STSI4203": {
+        "nama": "Interaksi Manusia dan Komputer",
+        "sks": 3,
+    },
+    "STSI4205": {
+        "nama": "Jaringan Komputer",
+        "sks": 3,
+    },
+
+    "MKDI4203": {
+        "nama": "Kewirausahaan di Era Digital",
+        "sks": 3,
+    },
+    "STSI4204": {
+        "nama": "Analisis dan Visualisasi Data",
+        "sks": 2,
+    },
+    "STSI4206": {
+        "nama": "Proses Bisnis",
+        "sks": 2,
+    },
+    "STSI4207": {
+        "nama": "Sistem Informasi Manajemen",
+        "sks": 3,
+    },
+    "STSI4208": {
+        "nama": "Analisis dan Perancangan Sistem",
+        "sks": 3,
+    },
+    "STSI4209": {
+        "nama": "Pemrograman Berbasis Web",
+        "sks": 3,
+    },
+    "EMBS4207": {
+        "nama": "Perilaku Organisasi",
+        "sks": 3,
+    },
+
+    "EMBS4102": {
+        "nama": "Pengantar Bisnis",
+        "sks": 3,
+    },
+    "FSAP4102": {
+        "nama": "Teori Organisasi",
+        "sks": 3,
+    },
+    "FSSI4101": {
+        "nama": "Basic Reading",
+        "sks": 3,
+    },
+    "STMA4111": {
+        "nama": "Kalkulus Diferensial",
+        "sks": 3,
+    },
+
+    "STSI4301": {
+        "nama": "Sistem Pendukung Keputusan",
+        "sks": 3,
+        "pilihan": True,
+    },
+    "STSI4302": {
+        "nama": "Administrasi Server",
+        "sks": 3,
+        "pilihan": True,
+    },
+    "STSI4303": {
+        "nama": "Pemrograman Berbasis Perangkat Bergerak",
+        "sks": 3,
+    },
+    "STSI4308": {
+        "nama": "Komunikasi Bisnis dan Teknis",
+        "sks": 2,
+    },
+
+    "EMBS4101": {
+        "nama": "Manajemen",
+        "sks": 4,
+    },
+    "MKKI4301": {
+        "nama": "Pemberdayaan Masyarakat",
+        "sks": 3,
+        "pilihan": True,
+    },
+    "STPL4211": {
+        "nama": "Sistem Informasi Perencanaan",
+        "sks": 3,
+        "pilihan": True,
+    },
+    "STSI4304": {
+        "nama": "Dasar Infrastruktur TI",
+        "sks": 2,
+    },
+    "STSI4305": {
+        "nama": "Etika Profesi",
+        "sks": 2,
+    },
+    "STSI4306": {
+        "nama": "Arsitektur dan Perancangan Sistem Enterprise",
+        "sks": 3,
+    },
+    "STSI4307": {
+        "nama": "Data Mining",
+        "sks": 3,
+        "pilihan": True,
+    },
+    "STSI4309": {
+        "nama": "Administrasi Jaringan",
+        "sks": 3,
+        "pilihan": True,
+    },
+    "STSI4310": {
+        "nama": "Metodologi Penelitian",
+        "sks": 3,
+    },
+
+    "STDA4101": {
+        "nama": "Pengantar Sains Data",
+        "sks": 3,
+    },
+
+    "STSI4402": {
+        "nama": "Tata Kelola Teknologi Informasi",
+        "sks": 2,
+    },
+    "STSI4403": {
+        "nama": "Manajemen Risiko dan Audit Sistem Informasi",
+        "sks": 3,
+    },
+    "STSI4404": {
+        "nama": "Keamanan Jaringan",
+        "sks": 3,
+        "pilihan": True,
+    },
+    "STSI4405": {
+        "nama": "Keamanan Sistem Informasi",
+        "sks": 2,
+    },
+    "STSI4406": {
+        "nama": "Manajemen Proyek Sistem Informasi",
+        "sks": 2,
+    },
+    "STSI4407": {
+        "nama": "Manajemen Layanan Teknologi Informasi",
+        "sks": 3,
+    },
+    "STSI4408": {
+        "nama": "E-Bisnis",
+        "sks": 3,
+    },
+    "STSI4409": {
+        "nama": "Data Warehouse",
+        "sks": 3,
+        "pilihan": True,
+    },
+    "STSI4440": {
+        "nama": "Capstone Project",
+        "sks": 6,
+        "pilihan": True,
+    },
+
+    "EACC4207": {
+        "nama": "Sistem Informasi Akuntansi",
+        "sks": 3,
+    },
+}v
