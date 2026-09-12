b6d62ca8ad8bcbdf66fec9071f4297ccc288819b
mismilum2008-ux <mis.milum2008@gmail.com>
2026-09-12 23:15:55 +0700

    Create references.py

---
+++ references.py
@@ -0,0 +1,31 @@
+# =========================================================
+# DATABASE REFERENSI MATA KULIAH UT
+# =========================================================
+
+COURSE_REFERENCES = {
+
+    # -----------------------------------------------------
+    # MKWN4109 - Pendidikan Kewarganegaraan
+    # -----------------------------------------------------
+
+    "MKWN4109": [
+        {
+            "jenis": "Modul UT",
+            "referensi": (
+                "Universitas Terbuka. (2025). "
+                "Pendidikan Kewarganegaraan (MKWN4109). "
+                "Tangerang Selatan: Universitas Terbuka."
+            ),
+        },
+        {
+            "jenis": "Buku",
+            "referensi": (
+                "Winarno. (2014). "
+                "Pembelajaran Pendidikan Kewarganegaraan: "
+                "Isi, Strategi, dan Penilaian. "
+                "Jakarta: Bumi Aksara."
+            ),
+        },
+    ],
+
+}
